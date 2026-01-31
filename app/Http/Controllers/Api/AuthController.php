fd15c0ff57b6797154bc4aea4c509934345d7604
SrgP2003 <sergioarepalacios9264@gmail.com>
2026-01-30 21:38:08 -0600

    Integracion de autenticacion con Laravel Sactum: migracion, modelo, controlador y definicion de rutas para su creacion

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //Funcion para iniciar sesion
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(
+                [
+                    'message' => 'Las credenciales ingresadas no son válidas'
+                ],
+                401
+            );
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+    //Funcion para cerrar sesion
+    public function logout(Request $request): JsonResponse{
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Sesión cerrada exitosamente'
+        ]);
+    }
+}
